a92908d2596024874ffc9823c6843e7210bd6e9a
hovgsky <hovgsky2133@gmail.com>
2025-06-16 21:56:13 +0200

    Add files via upload

---
+++ src/main/java/azure/Kalkulator/CalculationRequest.java
@@ -0,0 +1,13 @@
+package azure.Kalkulator;
+
+public class CalculationRequest {
+    private String expression;
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+}
